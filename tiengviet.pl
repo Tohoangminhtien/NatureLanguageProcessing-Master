7a4627327e932ab0fe9db0748c59e645c8aa7ad3
Tohoangminhtien <tientk17@gmail.com>
2023-07-01 12:22:34 +0700

    Chapter 2. Grammar

---
+++ tiengviet.pl
@@ -0,0 +1,25 @@
+sentence--> s, v.
+
+
+v-->verb.
+v-->verb, object.
+v-->verb, object, prep, object2.
+
+s--> [cooaays] | [anhaays] | [hoj].
+
+
+verb-->[ddi] | [lamf] | [nois].
+object-->[hues] | [baiftaapj] | [tooi].
+
+prep-->[vaof] | [bang].
+object2-->[xebus] | [ngayfmai].
+
+
+
+
+
+
+
+
+
+
